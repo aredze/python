88eb29ec815de95e41fe1ccc7dcbe1428a115852
andrea <aredze@gmail.com>
2019-02-09 18:31:23 -0800

    Moved some files from python/ to directory main/

---
+++ tmp/test-namespaces/a.py
@@ -0,0 +1,16 @@
+
+import argparse
+import b
+
+def f1():
+    print "pippo"
+
+def main():
+    argparse
+    parser = argparse.ArgumentParser("script1")
+    parser.add_arguments("ciao")
+    b.add_arguments(parser)
+    f1()
+
+if name=="__main__"
+    main()    
